567e54c9f96dace916925304ddf3d2f8548ceb39
Vlad <funnytrofi@gmail.com>
2022-08-06 00:05:55 +0600

    edition 2 chart

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -5,6 +5,8 @@ import 'package:intl/intl.dart';
 class Chart extends StatelessWidget {
   late final List<Transaction> recentTransactions;
 
+  Chart(this.recentTransactions);
+
   List<Map<String, Object>> get groupedTransactionValues {
     return List.generate(7, (index) {
       final weekDay = DateTime.now().subtract(
@@ -19,8 +21,12 @@ class Chart extends StatelessWidget {
           totalSum += recentTransactions[i].amount;
         }
       }
+
+      print(DateFormat.E().format(weekDay));
+      print(totalSum);
+
       return {
-        'day': DateFormat.E(weekDay),
+        'day': DateFormat.E().format(weekDay).substring(0, 1),
         'amount': totalSum,
       };
     });
@@ -28,11 +34,14 @@ class Chart extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    print(groupedTransactionValues);
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
       child: Row(
-        children: [],
+        children: groupedTransactionValues.map((data) {
+          return Text('${data['day']}: ${data['amount']}');
+        }).toList(),
       ),
     );
   }
